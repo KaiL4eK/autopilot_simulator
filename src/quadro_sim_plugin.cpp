fd90f32a0e43d40daba40417ba4c9a4a974110e4
Alex-SAU <avdevyatkin@etu.ru>
2017-09-19 15:00:49 +0300

    +u checked - force is Newtons

--- src/quadro_sim_plugin.cpp
+++ src/quadro_sim_plugin.cpp
@@ -15,6 +15,10 @@ namespace gazebo
             // Store the pointer to the model
             model   = _parent;
             link    = model->GetLink();
+            physics::WorldPtr world = model->GetWorld();
+            physics::PhysicsEnginePtr eng = world->GetPhysicsEngine();
+
+            eng->SetMaxStepSize(0.001);
 
             // Listen to the update event. This event is broadcast every
             // simulation iteration.
@@ -38,20 +42,23 @@ namespace gazebo
             math::Vector3   rot         = pose.rot.GetAsEuler();
             math::Vector3   linear_vel  = link->GetRelativeLinearVel();
             // printf("%f / %f / %f\n", pose.pos.x, pose.pos.y, pose.pos.z);
-            printf("%f / %f / %f / %f / %f\n", rot.x, rot.y, rot.z, rot.x * 180 / M_PI * SIGN(torque_x), torque_x);
-
+            // printf("%f / %f / %f / %f / %f\n", rot.x, rot.y, rot.z, rot.x * 180 / M_PI * SIGN(torque_x), torque_x);
+            printf("%f / %f / %f\n", linear_vel.x, linear_vel.y, linear_vel.z);
+            
             math::Vector3 force = link->GetRelativeForce();
             // printf("%f / %f / %f\n", force.x, force.y, force.z);
 
             // usleep( 1 * 1000 );
 
             
-
+            /*
             if ( rot.x * 180 / M_PI * SIGN(torque_x) > 10 )
                 torque_x *= -1;
-
+            
             link->AddRelativeTorque(math::Vector3(torque_x, 0, 0));
-            link->AddRelativeForce(math::Vector3(0, 0, 0));
+            */
+
+            link->AddRelativeForce(math::Vector3(0.01, 0, 0));  // [Newtons]
         }
 
         private:
