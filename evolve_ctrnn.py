20e4eacb053bd4f027fef544d4c12746a59aff6d
LETI <etu.ru>
2017-10-17 03:39:21 +0300

    +u some fixes

--- evolve_ctrnn.py
+++ evolve_ctrnn.py
@@ -68,7 +68,7 @@ def eval_genomes(genomes, config):
 
 def run():
     local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'config-feedforward')
+    config_path = os.path.join(local_dir, 'config-ctrnn')
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
